794b2dff18ca891704c52ec29d1684cbe400952a
Ronan <Ronan@RonanCorp>
2015-01-07 00:08:20 +0200

    Event model added. Not working code. Expected model for City, Country, PriceCategory. To make this code working comment class Event and related to it line in other classes

---
+++ hostme-persistence/src/main/java/com/softserve/edu/model/Event.java
@@ -0,0 +1,263 @@
+package com.softserve.edu.model;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import javax.persistence.*;
+
+/**
+ * 
+ * @author Ronan Class for events, which contain all fields related to
+ *         information about event
+ *
+ */
+@Entity
+@Table(name = "EVENTS", uniqueConstraints = { @UniqueConstraint(columnNames = "feedback_id") })
+public class Event {
+	@Id
+	@GeneratedValue
+	@Column(name = "event_id", unique = true, nullable = false)
+	private Integer eventId;
+
+	@Column(name = "description")
+	private String description;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "start_date")
+	private Calendar startDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "end_date")
+	private Calendar endDate;
+
+	@Column(name = "attendee")
+	private String attendee;
+
+	@ManyToOne
+	@JoinColumn(name = "city_id", nullable = false)
+	private City city;
+
+	@Column(name = "address")
+	private String address;
+
+	@Column(name = "owner")
+	// maybe I should use User
+	private String owner;
+
+	@ManyToOne
+	@JoinColumn(name = "state_id", nullable = false)
+	private State state;
+
+	@Column(name = "comment")
+	private String comment;
+
+	@ManyToOne
+	@JoinColumn(name = "price_id", nullable = false)
+	private PriceCategory priceCategory;
+
+	@Column(name = "website")
+	private String website;
+
+	@OneToMany(mappedBy = "event", fetch = FetchType.EAGER, orphanRemoval = true)
+	private Set<Image> image;
+
+	public Event() {
+		super();
+	}
+
+	public Integer getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(Integer eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Calendar endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getAttendee() {
+		return attendee;
+	}
+
+	public void setAttendee(String attendee) {
+		this.attendee = attendee;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public PriceCategory getPriceCategory() {
+		return priceCategory;
+	}
+
+	public void setPriceCategory(PriceCategory priceCategory) {
+		this.priceCategory = priceCategory;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public Set<Image> getImage() {
+		return image;
+	}
+
+	public void setImage(Set<Image> image) {
+		this.image = image;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result
+				+ ((attendee == null) ? 0 : attendee.hashCode());
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+		result = prime * result + ((eventId == null) ? 0 : eventId.hashCode());
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		result = prime * result
+				+ ((startDate == null) ? 0 : startDate.hashCode());
+		result = prime * result + ((website == null) ? 0 : website.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (attendee == null) {
+			if (other.attendee != null)
+				return false;
+		} else if (!attendee.equals(other.attendee))
+			return false;
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (endDate == null) {
+			if (other.endDate != null)
+				return false;
+		} else if (!endDate.equals(other.endDate))
+			return false;
+		if (eventId == null) {
+			if (other.eventId != null)
+				return false;
+		} else if (!eventId.equals(other.eventId))
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		if (website == null) {
+			if (other.website != null)
+				return false;
+		} else if (!website.equals(other.website))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [eventId=" + eventId + ", description=" + description
+				+ ", startDate=" + startDate + ", endDate=" + endDate
+				+ ", attendee=" + attendee + ", address=" + address
+				+ ", owner=" + owner + ", comment=" + comment + ", website="
+				+ website + ", image=" + image + "]";
+	}
+
+}
