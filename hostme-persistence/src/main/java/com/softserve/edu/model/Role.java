36f128deb1a9351bbe3690f27e576af8dd45160c
Ronan <Ronan@RonanCorp>
2015-01-07 15:46:39 +0200

    hashcode

--- hostme-persistence/src/main/java/com/softserve/edu/model/Role.java
+++ hostme-persistence/src/main/java/com/softserve/edu/model/Role.java
@@ -30,6 +30,41 @@ public class Role {
 	public void setRoleId(Integer roleId) {
 		this.roleId = roleId;
 	}
+
+	public Role() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((roleId == null) ? 0 : roleId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (roleId == null) {
+			if (other.roleId != null)
+				return false;
+		} else if (!roleId.equals(other.roleId))
+			return false;
+		return true;
+	}
 	
 	
 
