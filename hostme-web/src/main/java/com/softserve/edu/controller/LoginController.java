63bbf773cf4affdc0b751dab48f6ff86001c71eb
Vasyl <vasyl5995@gmail.com>
2015-01-08 15:56:04 +0200

    fixed build problem. sorry. now builds fine

--- hostme-web/src/main/java/com/softserve/edu/controller/LoginController.java
+++ hostme-web/src/main/java/com/softserve/edu/controller/LoginController.java
@@ -1,18 +1,13 @@
 package com.softserve.edu.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import com.softserve.edu.repositories.ConversationRepository;
-
 @Controller
 public class LoginController {
-//    @Autowired
-//    private ConversationRepository repository;
+    
 	@RequestMapping("/login")
 	public String login() {
-//	    System.out.println(repository.count());
 		return "login";
 	}
 }
