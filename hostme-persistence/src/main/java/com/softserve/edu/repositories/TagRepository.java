ca84455707cfd36a783a1a7abea88591cc4f110c
Alex13ss <newbeecon@mail.ru>
2015-01-08 16:36:07 +0200

    + repositories

---
+++ hostme-persistence/src/main/java/com/softserve/edu/repositories/TagRepository.java
@@ -0,0 +1,9 @@
+package com.softserve.edu.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.softserve.edu.model.Tag;
+
+public interface TagRepository extends CrudRepository<Tag, Long> {
+
+}
