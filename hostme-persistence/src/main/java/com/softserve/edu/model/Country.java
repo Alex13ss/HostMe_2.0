b7f730051864432ca4c2b0d1db1d853160d0ff3a
Ronan <Ronan@RonanCorp>
2015-01-08 13:32:43 +0200

    Country an City added

---
+++ hostme-persistence/src/main/java/com/softserve/edu/model/Country.java
@@ -0,0 +1,96 @@
+package com.softserve.edu.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "COUNTRIES", uniqueConstraints = { @UniqueConstraint(columnNames = "country_id") })
+public class Country {
+	@Id
+	@GeneratedValue
+	@Column(name = "country_id", unique = true, nullable = false)
+	private Integer countryId;
+/*	
+	@Column(name = "country", length = 32)
+	private String country;
+	
+	@OneToMany(mappedBy = "country", fetch = FetchType.EAGER, orphanRemoval = true)
+	private Set<City> city;
+
+	public Country() {
+		super();
+	}
+
+	public Integer getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(Integer countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Set<City> getCity() {
+		return city;
+	}
+
+	public void setCity(Set<City> city) {
+		this.city = city;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result
+				+ ((countryId == null) ? 0 : countryId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Country other = (Country) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (countryId == null) {
+			if (other.countryId != null)
+				return false;
+		} else if (!countryId.equals(other.countryId))
+			return false;
+		return true;
+	}
+	
+	*/
+	
+}
