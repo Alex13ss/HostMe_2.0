5c6d17fb2954d09267854ebeacd73960e50ddf0c
Vasyl <vasyl5995@gmail.com>
2015-01-08 11:51:59 +0200

    Conversation Entity

--- hostme-persistence/src/main/java/com/softserve/edu/model/Conversation.java
+++ hostme-persistence/src/main/java/com/softserve/edu/model/Conversation.java
@@ -7,28 +7,26 @@ import javax.persistence.*;
 
 @Entity
 public class Conversation {
-	@Id
-	@GeneratedValue
-	private Long id;
-	/*
-	@Column(name = "name")
-	private String name;
-	
-	@Column(name = "createdAt")
-	private Date createdAt;
-	
-	@ManyToOne
-	@JoinColumn(name = "group_id")
-	private Group group;
-	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User ownerUser;
-	
-	@OneToMany()
-	private Set<User> moderators;
-	
-	@OneToMany(mappedBy = "conversation_id")
-	private Set<Post> posts;
-	*/
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+    // @ManyToOne
+    // @JoinColumn(name = "group_id")
+    // private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User ownerUser;
+    /*
+     * @OneToMany() private Set<User> moderators;
+     * 
+     * @OneToMany(mappedBy = "conversation_id") private Set<Post> posts;
+     */
 }
