76243629ba071c2dbaccf753ff4324d2a347419d
Alex13ss <newbeecon@mail.ru>
2015-01-08 13:52:58 +0200

    Group & Tag entities

---
+++ hostme-persistence/src/main/java/com/softserve/edu/model/Tag.java
@@ -0,0 +1,35 @@
+package com.softserve.edu.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Tag {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "tag_id", unique = true, nullable = false)
+    private Long tagId;
+    
+    @Column(name = "tag_name", nullable = false)
+    private String tagName;
+
+    public Long getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(Long tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+    
+}
