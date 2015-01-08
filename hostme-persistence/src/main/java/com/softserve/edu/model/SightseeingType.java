4a1a846f6a1013782fabba29913ba44286325368
blutto90 <pavlusha9@gmail.com>
2015-01-09 00:45:08 +0200

    persistence and dao layer for sightseeing

---
+++ hostme-persistence/src/main/java/com/softserve/edu/model/SightseeingType.java
@@ -0,0 +1,5 @@
+package com.softserve.edu.model;
+
+public enum SightseeingType {
+	PARK, MUSEUM, MONUMENT, EXHIBITION, THEATRE
+}
---
+++ hostme-service/src/main/java/com/softserve/edu/service/SightseeingService.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.service;
+
+import java.util.Set;
+
+import com.softserve.edu.model.Sightseeing;
+
+public interface SightseeingService {
+	Set<Sightseeing> findAll();
+	
+}
