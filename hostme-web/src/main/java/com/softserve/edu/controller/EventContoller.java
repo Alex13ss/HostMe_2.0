cabd63f394bbe753b814d8e907a762c199f02120
Ronan <Ronan@RonanCorp>
2015-01-10 14:18:24 +0200

    Controller and an empty jsp

---
+++ hostme-web/src/main/java/com/softserve/edu/controller/EventContoller.java
@@ -0,0 +1,33 @@
+package com.softserve.edu.controller;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.softserve.edu.model.Event;
+import com.softserve.edu.service.CityService;
+import com.softserve.edu.service.CountryService;
+import com.softserve.edu.service.EventService;
+
+@Controller
+public class EventContoller {
+	
+	@Autowired
+	EventService eventService;
+	
+	
+	
+	@RequestMapping(value = "/event", method = RequestMethod.GET)
+	public String getEvents(Model model){
+		List<Event> events = eventService.getAllEvents();
+		System.out.println("ASDD");
+		model.addAttribute("events", events);
+		return "events";
+		
+	}
+}
