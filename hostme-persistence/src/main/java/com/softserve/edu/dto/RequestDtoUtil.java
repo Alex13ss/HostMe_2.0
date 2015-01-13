ee1886d7e06d37b965ee79c51085db77d46e3656
Ronan <Ronan@RonanCorp>
2015-01-13 23:22:59 +0200

    Added after deleting

---
+++ hostme-persistence/src/main/java/com/softserve/edu/dto/RequestDtoUtil.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.dto;
+
+import com.softserve.edu.model.Request;
+import com.softserve.edu.model.Status;
+import com.softserve.edu.model.exceptions.RequestCannotSendException;
+
+public interface RequestDtoUtil {
+	public Request formRequestModel(long endDate,long beginDate,Status status,int hostingId,String userLogin,String notes) throws RequestCannotSendException;
+
+}
